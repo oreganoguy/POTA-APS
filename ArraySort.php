2af01ea9d5d75906f265bff6f1574b6ad7fd40a3
Nathan Carnelos <nathancoltinho@gmail.com>
2018-11-17 00:44:49 -0300

    adicionado metodo do radix, limpa no index

--- ArraySort.php
+++ ArraySort.php
@@ -1,7 +1,14 @@
 <?php 
+
+interface iArraySort {
+    function bubble(array $array);
+    function quick(array $array);
+    function radix(array $array);
+}
+
 class ArraySort {
 
-    public function bublee($array) {
+    protected function bubble($array) {
         $count = 0;
         for($i = 0; $i < count($array); $i++)
         {
@@ -19,32 +26,53 @@ class ArraySort {
         return $count;
     }
 
-    public function select($val) {
-        return $val;
-    }
-    public function insertion($val) {
-        return $val;
-    }
-    public function heap($val) {
-        return $val;
-    }
-    public function merge($val) {
-        return $val;
-    }
-    public function quick($val) {
+    protected function quick($val) {
         return $val;
     }
 
-    public function count($val) {
-        return $val;
-    }
+    protected function radix($elements) {
+        $count = 0;
+        // Array for 10 queues.
+        $queues = array(
+            array(), array(), array(), array(), array(), array(), array(), array(),
+            array(), array()
+        );
+        // Queues are allocated dynamically. In first iteration longest digits
+        // element also determined.
+        $longest = 0;
+        foreach ($elements as $el) {
+            if ($el > $longest) {
+            $longest = $el;
+            }
+            $count++;
 
-    public function bucket($val) {
-        return $val;
-    }
-
-    public function radix($val) {
-        return $val;
+            //array_push adiciona valor ao final do array.,
+            array_push($queues[$el % 10], $el);
+        }
+        // Queues are dequeued back into original elements.
+        $i = 0;
+        foreach ($queues as $key => $q) {
+            while (!empty($queues[$key])) {
+                // array_shift remove o primeiro valor do array.
+            $elements[$i++] = array_shift($queues[$key]);
+            }
+        }
+        // Remaining iterations are determined based on longest digits element.
+        $it = strlen($longest) - 1;
+        $d = 10;
+        while ($it--) {
+            foreach ($elements as $el) {
+            array_push($queues[floor($el/$d) % 10], $el);
+            }
+            $i = 0;
+            foreach ($queues as $key => $q) {
+                while (!empty($queues[$key])) {
+                    $elements[$i++] = array_shift($queues[$key]);
+                }
+            }
+            $d *= 10;
+        }
+        return $count;
     }
 }
 
--- index.php
+++ index.php
@@ -1,54 +1,40 @@
 <?php
 include('ArraySort.php');
 
-class main extends ArraySort {
+interface iMain {
+    public function counter (int $size);
+}
 
-    public function counter ($method,$init, $Final) {
-        $count = 0;
+class Main extends ArraySort implements iMain {
+
+    public function counter (int $size) {
+        echo '</br>';
+        $response = ['bubble' => 0, 'quick'=> 0, 'radix' => 0];
         for ($i = 0; $i < 50; $i++) {
-            $count += $this->$method(arrayCreator(0,4));
-        }
+            $response['bubble'] += $this->bubble($this->randomArrayCreator($size));
+        }       
+        echo '</br> Bubble: '.($response['bubble']/50).'</br>';
+
+        // for ($i = 0; $i < 50; $i++) {
+        //     $response['quick'] += $this->quick($this->randomArrayCreator($size));
+        // }       
+        // echo '</br>'.($response['quick']/50).'</br>';
+
+        for ($i = 0; $i < 50; $i++) {
+            $response['radix'] += $this->radix($this->randomArrayCreator($size));
+        }       
+        echo '</br> Radix: '.($response['radix']/50).'</br>';
     }
+
+    private function randomArrayCreator (int $size) : array{
+        $size -= 1;
+        $init = 0;
+        $array = range($init, $size);
+        shuffle($array);
+        return $array;
+    } 
 }
 
-
-$teste = new main();
+$main = new Main();
 $count = 0;
-
-for ($i = 0; $i < 50; $i++) {
-    $count += $teste->bublee(arrayCreator(0,4));
-}
-echo 'bublee 5: '.$count."\n"; 
-
-$count=0;
-for ($i = 0; $i < 50; $i++) {
-    $count += $teste->bublee(arrayCreator(0,9));
-}
-echo 'bublee 10: '.$count; 
-exit;
-
-function arrayCreator ($init, $final) {
-    $array = range($init, $final);
-    shuffle($array);
-    return $array;
-} 
-
-/*
-$ArraySort = new ArraySort();
-$count = 0;
-for ($i = 0; $i < 50; $i++) {
-    $count += $ArraySort->bublee(arrayCreator(0,4));
-}
-echo 'bublee 5'.$count."\n"; 
-$count=0;
-for ($i = 0; $i < 50; $i++) {
-    $count += $ArraySort->bublee(arrayCreator(0,9));
-}
-echo 'bublee 10'.$count; 
-
-
-echo'<pre>';var_dump($array);
-
-echo'----------------------------------';
-echo'<pre>';var_dump($array);
-*/
+$main->counter(10);
