d20ab2a2c327decdf8b43cb18006bfdfb319f039
oreganoguy <jhonnyrios@hotmail.com.br>
2018-11-18 00:44:47 -0200

    Adding a new function
    QuickSort

--- ArraySort.php
+++ ArraySort.php
@@ -26,8 +26,38 @@ class ArraySort {
         return $count;
     }
 
-    protected function quick($val) {
-        return $val;
+    protected function quick($array) {
+   	// find array size
+	$length = count($array);
+	
+	// base case test, if array of length 0 then just return array to caller
+	if($length <= 1){
+		return $array;
+	}
+	else
+    {
+	
+		// select an item to act as our pivot point, since list is unsorted first position is easiest
+		$pivot = $array[0];
+		
+		// declare our two arrays to act as partitions
+		$left = $right = array();
+		
+		// loop and compare each item in the array to the pivot value, place item in appropriate partition
+		for($i = 1; $i < count($array); $i++)
+		{
+			if($array[$i] < $pivot){
+				$left[] = $array[$i];
+			}
+			else{
+				$right[] = $array[$i];
+			}
+		}
+		
+		// use recursion to now sort the left and right lists
+		return array_merge(quick_sort($left), array($pivot), quick_sort($right));
+	}
+        return $array;
     }
 
     protected function radix($elements) {
